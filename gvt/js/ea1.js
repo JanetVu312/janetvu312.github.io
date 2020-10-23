27e4bac52e631904533d5ebc4732beff7e67b393
JanetVu312 <janet.vu@stud.fh-luebeck.de>
2020-10-23 21:10:07 +0200

    Update ea1.js

--- gvt/js/ea1.js
+++ gvt/js/ea1.js
@@ -116,9 +116,9 @@ function stopAnimate() {
 
 function animateScript() {
 
-  var position = 450; //520 Startposition
-  const interval = 200; //Interval in ms
-  const diff = 450; //Diff 520
+  var position = 520; //520 Startposition
+  const interval = 250; //Interval in ms
+  const diff = 520; //Diff 520
 
   tID = setInterval(() => {
 
